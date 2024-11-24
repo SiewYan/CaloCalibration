20b8327debdbc88781d9c3a01f8cf8c464376405
SiewYan <siew.yan.hoh@gmail.com>
2024-11-24 16:40:46 +0800

    mod

--- scripts/mk-skim-list.sh
+++ scripts/mk-skim-list.sh
@@ -4,6 +4,10 @@
 #ls /lustre/collider/siew/pruned_datasets/RW_Run4F/*.root > /home/siew/gm2/df-spectrum/data/skim/rw_run4f.txt
 #ls /lustre/collider/siew/pruned_datasets/RE_Run4F/*.root > /home/siew/gm2/df-spectrum/data/skim/re_run4f.txt
 
+# skim file for spacer
+# /lustre/collider/siew/study_v2/spacer
+DIRECTORY="/lustre/collider/siew/study_v2/spacer"
+
 CWD=`pwd`
 
 TEST=$(echo $CWD | awk -F "/" '{print $NF}')
@@ -17,12 +21,12 @@ fi
 for ioffset in offset-0p00mm  offset-12p5mm  offset-25p0mm  offset-37p5mm  offset-50p0mm  offset-62p5mm  offset-75p0mm
 do
     echo creating list ${CWD}/data/${ioffset}.txt
-    ls /lustre/collider/siew/study_v2/spacer/art-skim/${ioffset}/*/data/*.root > ${CWD}/data/${ioffset}.txt
+    ls ${DIRECTORY}/art-skim/${ioffset}/*/data/*.root > ${CWD}/data/${ioffset}.txt
 done
 
 # make Data
 for irun in run60787  run60788  run60789  run60791  run60866  run60867  run60868  run60869  run60870  run60871
 do
     echo creating list ${CWD}/data/${irun}.txt
-    ls /lustre/collider/siew/study_v2/spacer/data/${irun}/*/*/data/*.root > ${CWD}/data/${irun}.txt
+    ls ${DIRECTORY}/data/${irun}/*/*/data/*.root > ${CWD}/data/${irun}.txt
 done
