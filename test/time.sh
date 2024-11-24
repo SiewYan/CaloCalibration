1aff649fafdef352f3727e8e5ee77a8e8ddfcc74
SiewYan <siew.yan.hoh@gmail.com>
2024-11-24 20:59:59 +0800

    fix time

--- test/time.sh
+++ test/time.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-#./bin/time -f /home/siew/gm2/df-spectrum/data/spacer/fit-result/fit_data-spacer.root -t 60 -t 600
-#./bin/time -f /home/siew/gm2/df-spectrum/data/spacer/fit-result/fit_data-reference.root -t 60 -t 600
-#./bin/time -f /home/siew/gm2/df-spectrum/data/spacer/fit-result -t 0 -t 500
+# DATA
+./bin/time -f /home/siew/CaloCalibration/data/fit-results/reference_run.root -t 0 -t 500 
 
-# run4
-./bin/time -f /home/siew/gm2/df-spectrum/data/materials/fit-result/fit_run4F_rw.root -t 60 -t 600
-./bin/time -f /home/siew/gm2/df-spectrum/data/materials/fit-result/fit_run4O_rw.root -t 60 -t 600
+# MC
+./bin/time -f /home/siew/CaloCalibration/data/fit-results/offset-0p00mm.root -t 60 -t 600
 
-# run5
-./bin/time -f /home/siew/gm2/df-spectrum/data/materials/fit-result/fit_run5A_rw.root -t 60 -t 600
-./bin/time -f /home/siew/gm2/df-spectrum/data/materials/fit-result/fit_run5BC_rw.root -t 60 -t 600
-./bin/time -f /home/siew/gm2/df-spectrum/data/materials/fit-result/fit_run5P_rw.root -t 60 -t 600
-
-# mc
-./bin/time -f /home/siew/gm2/df-spectrum/data/materials/fit-result/fit_gasgun.root -t 0 -t 500
-
-
-# RE
-./bin/time -f /home/siew/gm2/df-spectrum/data/materials/fit-result/fit_run4F_rw.root -t 60 -t 600
-./bin/time -f /home/siew/gm2/df-spectrum/data/materials/fit-result/fit_run5A_rw.root -t 60 -t 600
